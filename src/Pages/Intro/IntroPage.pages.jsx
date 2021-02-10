364d7d61d6646cc2ef17d36f8bfe89809ef6149b
Branden Chong <bchong12@g.ucla.edu>
2021-02-09 17:00:53 -0800

    setup

---
+++ src/Pages/Intro/IntroPage.pages.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./IntroPage.pages.css";
+
+const IntroPage = () => {
+    return (
+        <div className="introPage">
+            <div className="innerDiv">
+                <div className="logo"></div>
+            </div>
+        </div >
+    )
+}
+
+export default IntroPage
